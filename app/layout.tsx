b5c8d09dd251f4f60799bcf7f713340f23877efd
T-umiyama <151260401+T-umiyama@users.noreply.github.com>
2025-08-08 14:31:35 +0900

    chore: remove vulnerable analytics deps

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +18,6 @@ export default function RootLayout({
     <html lang="ja">
       <body className={inter.className}>
         {children}
-        <SpeedInsights />
-        <Analytics />
       </body>
     </html>
   );
